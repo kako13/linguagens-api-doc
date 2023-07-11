927491a266b6e61b4289336d43a8c755be81dbac
Kauê Oliveira <kso13@hotmail.com>
2023-07-11 12:55:30 -0300

    deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          excludeLanguages: ['en'],
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
